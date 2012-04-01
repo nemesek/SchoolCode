6a9abcb2b19b767323d86f56196ffde233d3b4b5
Dan Nemesek <dnemesek@gmail.com>
2012-03-31 20:53:53 -0500

    adding sql script file for data population

---
+++ CS475/ProjectScript.sql
@@ -0,0 +1,24 @@
+use database1;
+INSERT INTO Students (FirstName, LastName, Age, School, RiskAssessment, AdministratorID) VALUES ('Adrian', 'Peterson', 18, 'Palestine TX', 5, 1);
+INSERT INTO Students (FirstName, LastName, Age, School, RiskAssessment, AdministratorID) VALUES ('Barry', 'Sanders', 16, 'KansasCity KA', 2, 2);
+INSERT INTO Students (FirstName, LastName, Age, School, RiskAssessment, AdministratorID) VALUES ('Earl', 'Campbell', 18, 'Tyler TX', 5, 1);
+
+INSERT INTO Questions (Type, Value, Test, Color) VALUES ('Radio6', 'How often are you so nervous or anxioius that you can not sit still?', 'SYSR', 'Yellow');
+INSERT INTO Questions (Type, Value, Test, Color) VALUES ('Radio6', 'How often do you feel that you are in control of your life?', 'SYSR', 'Yellow');
+INSERT INTO Questions (Type, Value, Test, Color) VALUES ('Radio6', 'How often do you feel hopeless about your life?', 'SYSR','Yellow');
+INSERT INTO Questions (Type, Value, Test, Color) VALUES ('Radio6', 'During the last 24 hours, how many times have you though of suicide?', 'SYSR','Purple');
+INSERT INTO Questions (Type, Value, Test, Color) VALUES ('Radio6', 'In the last 2 weeks, how many times have you thought of suicide?', 'SYSR','Purple');
+INSERT INTO Questions (Type, Value, Test, Color) VALUES ('Radio6', 'How much do these thoughts of suicide interfere with your daily life, for example with school work, having fun, doing things with you friends?', 'SYSR','Green');
+INSERT INTO Questions (Type, Value, Test) VALUES ('Radio2', 'Have you ever thought about suicide?', 'SYSR');
+INSERT INTO Questions (Type, Value, Test) VALUES ('Radio2', 'Have you ever thought about HOW you would try to end your life?', 'SYSR');
+INSERT INTO Questions (Type, Value, Test) VALUES ('Radio2', 'Are you thinking now about HOW you would try to end your life?', 'SYSR');
+INSERT INTO Questions (Type, Value, Test) VALUES ('Radio2', 'Has a friend or family member attempted or completed suicide?', 'SYSR');
+INSERT INTO Questions (Type, Value, Test) VALUES ('Radio5', 'When did this happen?', 'SYSR');
+INSERT INTO Questions (Type, Value, Test) VALUES ('InputText', 'To whom?', 'SYSR');
+INSERT INTO Questions (Type, Value, Test) VALUES ('Radio2', 'Have you ever made a suicide attempt?', 'SYSR');
+INSERT INTO Questions (Type, Value, Test, Color) VALUES ('Radio6', 'How many times have you made an attempt?', 'SYSR', 'Purple');
+INSERT INTO Questions (Type, Value, Test) VALUES ('InputText', 'What did you do? (How did you try to end your life?)', 'SYSR');
+INSERT INTO Questions (Type, Value, Test) VALUES ('Radio2', 'Was the attempt made when using drugs/alcohol?', 'SYSR');
+INSERT INTO Questions (Type, Value, Test, Color) VALUES ('Radio6', 'Compared to when you attempted suicide, how much more emotional pain are you in now?', 'SYSR', 'Green');
+
+
