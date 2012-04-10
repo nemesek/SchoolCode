5b579052df3e9c28137e72c2618e777de5d74e1d
Dan Nemesek <dnemesek@gmail.com>
2012-04-10 12:52:00 -0500

    Adding updated insert statement for TestQuestion.php

--- CS475/TestQuestion.php
+++ CS475/TestQuestion.php
@@ -1,4 +1,5 @@
 <?php
+
 $StudentID = $_GET['studentid'];
 $TestID = $_GET['testid'];
 $QuestionID = $_GET['questionid'];
@@ -8,25 +9,41 @@ $dbname="database1";
 $chandle = mysql_connect("localhost", $dbuser, $dbpass)
 	or die("Connection Failure to Database");
 mysql_select_db($dbname, $chandle) or die ($dbname . " Database not found. " . $dbuser);
+if(isset($_POST['BtnSubmit']))
+{
+  //echo "</br>Answer:{$_POST['number']}";
+  $answer = $_POST['number'];
+  echo $answer ."<BR>"; 
+  $prevQuestion = $QuestionID - 1;
+  $insert="INSERT INTO Answers (Value, TestID, QuestionID) VALUES (" .$answer ."," .$TestID ."," .$prevQuestion .")";
+  mysql_query($insert);	
+}
+
 $query="SELECT Value FROM Questions WHERE QuestionID=" .$QuestionID;
 echo "<BR>";
 $result = mysql_query($query);
-echo $result;
 $row = mysql_fetch_assoc($result);
 echo "<BR>";
 echo $row['Value'];
 $QuestionID = $QuestionID + 1;
 $next = $QuestionID;
-echo $QuestionID;
-$queryString = "http://localhost/TestQuestion.php?studentid=".$StudentID ."&testid=" .$TestID ."&questionid=" .$next;
+if($next < 19)
+{
+	$queryString = "http://localhost/TestQuestion.php?studentid=".$StudentID ."&testid=" .$TestID ."&questionid=" .$next;
+}
+else
+{
+	$queryString = "http://localhost/EndTest.php";
+}
 ?>
+
 <form name="Answer" method="POST" action="<?php echo $queryString; ?>"><br/><br/>
-	<input name="1" type="radio" value="1">
-	<input name="2" type="radio" value="2">
-	<input name="3" type="radio" value="3">
-	<input name="4" type="radio" value="4">
-	<input name="5" type="radio" value="5">
-	<input name="6" type="radio" value="6">
+	<input name="number" type="radio" value="1"> 1
+	<input name="number" type="radio" value="2"> 2
+	<input name="number" type="radio" value="3"> 3
+	<input name="number" type="radio" value="4"> 4
+	<input name="number" type="radio" value="5"> 5
+	<input name="number" type="radio" value="6"> 6
 	<br/><br/>
 	<input name="BtnSubmit" type="submit" value="Submit">
 </form>
