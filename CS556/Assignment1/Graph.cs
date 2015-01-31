93ffddd139e0fac36e8d068758158f13144f27aa
Nemesek <dnemesek@gmail.com>
2015-01-30 19:49:28 -0600

    first commit for assignment 1

---
+++ CS556/Assignment1/Graph.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    public class Graph<T>
+    {
+        private List<Vertex<T>> _list = new List<Vertex<T>>();
+        public Graph()
+        {
+
+        }
+
+        public Graph<T> AddVertex(Vertex<T> vertex, T label)
+        {
+            vertex.Label = label;
+            _list.Add(vertex);
+            return this;
+        }
+    }
+}
---
+++ CS556/Assignment1/Vertex.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    public class Vertex<T>
+    {
+        T _data;
+        public T Label  
+        {
+            get { return _data; }
+            set { _data = value; }
+        }
+    }
+}
