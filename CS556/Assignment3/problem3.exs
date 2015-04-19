7310888bcd4d5305d67c6a769baf2289bc9f8bb9
Nemesek <dnemesek@gmail.com>
2015-04-19 14:26:36 -0500

    getting closer - need to implement m messages

--- CS556/Assignment3/problem3.exs
+++ CS556/Assignment3/problem3.exs
@@ -1,30 +1,42 @@
 defmodule Star do
   def process() do
+    IO.puts "Got Spawned"
     receive do
-      {:ok,msg} -> IO.puts "MSG: #{msg}"
+       {msg} ->
+        IO.puts "Got a message #{msg}"
+        #send sender,{me,{:ok,"#{msg}"}}
       {:shutdown} -> exit(:boom)
     end
   end
 
-  def run(n,m) do _run(n,m) end
-  defp _run(0,_) do
+  def run(n,m) do _run(n,m,[]) end
+  defp _run(0,m,pids) do
      IO.puts "DONE SPAWNING"
+     pids
+     |> Enum.map(fn(p) -> sendMessages(m,p,self()) end)
+     IO.puts "Done"
   end
-  defp _run(n,m) do
+  
+  defp _run(n,m,pids) do
     Process.flag(:trap_exit,true)
     pid = spawn_link(Star,:process,[])
-    #send pid, {:ok, "Blah Blah"}
-    sendMessages(m,pid)
-    _run(n-1,m)
+    _run(n-1,m,[pid|pids])
   end
 
-  def sendMessages(m, pid) do _sendMessages(m, pid, "Blah") end
-  defp _sendMessages(0, pid, _) do
+  def handleCallback(pid) do
+    receive do
+      {^pid,{:ok,message}} -> IO.puts "Called back #{message}"
+    end
+  end
+
+  def sendMessages(m, pid, sender) do _sendMessages(m, pid, "Blah",sender) end
+  defp _sendMessages(0, pid, _,_) do
     send pid, {:shutdown}
   end
-  defp _sendMessages(m, pid, msg) do
-    send pid, {:ok, msg}
-    _sendMessages(m-1,pid,msg)
+  defp _sendMessages(m, pid, msg,sender) do
+    send pid,{"Blah"}
+    #handleCallback(pid)
+    _sendMessages(m-1,pid,msg,sender)
   end
 end
 
