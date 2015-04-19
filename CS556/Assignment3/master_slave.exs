c316fbe46aaadd6027000081f13050ea52c7e49c
Nemesek <dnemesek@gmail.com>
2015-04-19 16:08:57 -0500

    starting to send slave messages

--- CS556/Assignment3/master_slave.exs
+++ CS556/Assignment3/master_slave.exs
@@ -1,8 +1,22 @@
 defmodule MS do
-  def start(n) do _run(n,[])
+  def start(n) do
+     pids = _run(n,[])
+     IO.puts Enum.count(pids)
+     _setupCallback(pids)
   end
 
-  defp _run(0,_) do IO.puts "Started them all" end
+  defp _setupCallback(pids) do
+    receive do
+      {p, msg} ->
+        IO.puts "Got message #{msg}"
+        pid = Enum.at(pids,p-1)
+        send pid,{msg}
+        _setupCallback(pids)
+      end
+    IO.puts "Done setting them up"
+  end
+
+  defp _run(0,pids) do pids end
   defp _run(n, pids) do
     Process.flag(:trap_exit,true)
     pid = spawn_link(MS,:slave,[])
@@ -11,5 +25,12 @@ defmodule MS do
 
   def slave do
     IO.puts "Started"
+    receive do
+      {msg} -> IO.puts "Processed: #{msg}"
+      slave
+    end
   end
 end
+
+# pid = spawn(MS,:start,[5])
+# send pid, {2,"Hello"}
