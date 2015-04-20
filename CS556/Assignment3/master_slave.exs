20712b7183e19b1381de760a88c371c942176cb1
Nemesek <dnemesek@gmail.com>
2015-04-19 21:25:44 -0500

    sending a message back to master before exit

--- CS556/Assignment3/master_slave.exs
+++ CS556/Assignment3/master_slave.exs
@@ -8,24 +8,20 @@ defmodule MS do
   end
 
   def to_slave(msg,n, master) do
-    IO.puts "Got message #{msg} relaying to slave #{n}"
     send master, {n,msg}
   end
 
   defp _setupCallback(pids) do
     receive do
-      {p, "die"} ->
-        IO.puts "Routing die message"
+      {p,:normal} ->
         pid = Enum.at(pids,p-1)
-        send pid,{:shutdown}
         newPid = pid = spawn_link(MS,:slave,[])
         newPids = List.replace_at(pids,p-1,newPid)
         IO.puts "Initialized new process for N = #{p}"
         _setupCallback(newPids)
       {p, msg} ->
-        IO.puts "Router sending message #{msg}"
         pid = Enum.at(pids,p-1)
-        send pid,{msg}
+        send pid,{self,{msg, p}}
         _setupCallback(pids)
       end
   end
@@ -39,8 +35,10 @@ defmodule MS do
 
   def slave do
     receive do
-      {:shutdown} -> exit(:normal)
-      {msg} -> IO.puts "Slave Processed: #{msg}"
+      {sender,{"die",p}} ->
+        send sender,{p,:normal}
+        exit(:normal)
+      {_,{msg,_}} -> IO.puts "Slave Processed: #{msg}"
       slave
     end
   end
